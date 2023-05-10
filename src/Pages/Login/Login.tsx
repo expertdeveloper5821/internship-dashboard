7262c17fab956a98b1a6fe4ef3d6d7ad2b33bba8
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-05-10 18:38:04 +0530

    bug fix

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./Login.module.scss";
 import technologo from "../../Images/Technologo.png";
-import eyepassword from "../../Images/view.png";
+import girlImage from "../../Images/kindpng.png";
+import gradient from "../../Images/Ellipse 179.png";
+import { AiOutlineEye } from "react-icons/ai";
 
 type Props = {};
 
@@ -10,59 +12,72 @@ const Login = (props: Props) => {
     <div className={styles.maincontainer}>
       <div className={styles.bgcontainer}>
         <div className={styles.container}>
+          <div className={styles.wrapper}></div>
           <div className={styles.logo}>
             <img src={technologo} alt="Tg-logo"></img>
           </div>
 
-          <div className={styles.contentwrapper}>
+          {/* Para reset */}
+          <div>
             <h2>Welcome Back</h2>
             <p className={styles.parareset}>
-              Welcome back! Please enter your detailss
+              Welcome back! Please enter your details
             </p>
+          </div>
 
-            <div className={styles.inputdescription}>
-              <label className={styles.email}>Email</label>
-              <br></br>
-              <input
-                type="email"
-                className={styles.emaildesc}
-                placeholder="Enter Email"
-              ></input>
-            </div>
+          {/* Email Tag */}
+          <div className={styles.inputdescription}>
+            <label className={styles.email}>Email</label>
+            <br></br>
+            <input
+              type="email"
+              className={styles.emaildesc}
+              placeholder="Enter Email"
+            ></input>
+          </div>
 
-            <div className={styles.inputdesc}>
-              <label className={styles.password}>Password</label>
-              <br></br>
+          {/* Password Tag */}
+          <div className={styles.inputdescription}>
+            <label className={styles.password}>Password</label>
+            <br></br>
+            <input
+              type="password"
+              className={styles.passworddesc}
+              placeholder="Enter password"
+            ></input>
+            {/* <div className={styles.eyepassword}>
+              <AiOutlineEye color="#CC85858" size={20} />
+            </div> */}
+          </div>
 
-              <div className={styles.inputtype}>
-                <input
-                  type="password"
-                  className={styles.passworddesc}
-                  placeholder="Enter password"
-                ></input>
+          {/* Remember me  */}
+          <div className={styles.remembercheck}>
+            <input type="checkbox"></input>
+            <span>Remember for 30 days</span>
+          </div>
 
-                <div className={styles.eyepassword}>
-                  <img src={eyepassword} alt="eye-passwod"></img>
-                </div>
-              </div>
-            </div>
+          {/* Sign in */}
+          <div className={styles.buttoncontainer}>
+            <button type="submit" className={styles.forgetbutton}>
+              Sign in
+            </button>
+          </div>
 
-            <div className={styles.remembercheck}>
-              <input type="checkbox"></input>
-              <span>Remember for 30 days</span>
-            </div>
-
-            <div className={styles.buttoncontainer}>
-              <button type="submit" className={styles.forgetbutton}>
-                Signup
-              </button>
-            </div>
-
-            <div className={styles.signin}>
-              <span>
-                <a href="/resetpassword"> Forget your Password?</a>
-              </span>
-            </div>
+          {/* href Tag */}
+          <div className={styles.signin}>
+            <span>
+              <a href="/resetpassword"> Forget your Password?</a>
+            </span>
+          </div>
+        </div>
+        <div className={styles.girlSection}>
+          <img
+            src={girlImage}
+            alt="girl_image"
+            className={styles.girlimgs}
+          ></img>
+          <div className={styles.linearBottom}>
+            <img src={gradient} alt="gradient"></img>
           </div>
         </div>
       </div>
