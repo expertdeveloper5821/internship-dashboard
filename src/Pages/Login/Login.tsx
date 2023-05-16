1695bf419f8948718a6cf2ca3fe056af726b9727
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-05-16 17:38:33 +0530

    addbackgroundimage

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -74,7 +74,7 @@ const Login = (props: Props) => {
   }, [storedemail, storedPassword, setFieldValue]);
 
   return (
-    <div className={styles.maincontainer}>
+    <div className={styles.main_container}>
       <div className={styles.background_container}>
         <div className={styles.container}>
           <div className={styles.logo}>
