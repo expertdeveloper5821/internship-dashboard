47b085d28598b0bcd6f489053c003948285c030e
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-05-16 16:43:54 +0530

    bugs updated

--- src/Pages/Login/SentMail.tsx
+++ src/Pages/Login/SentMail.tsx
@@ -7,31 +7,35 @@ export function SentMail() {
     <>
       <div className={styles.maincontainer}>
         <div className={styles.background_container}>
-          <div className={styles.container}>
+          <div className={styles.container} style={{ border: "1px solid" }}>
             <div className={styles.logo}>
               <img src="./assets/Tehnogeticlogo.svg" alt="Tg-logo"></img>
             </div>
 
-            {/* Para reset */}
-            <div>
-              <h2
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
+            <div
+              className="heading_wrapper"
+              style={{
+                border: "1px solid black",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                margin: "0px",
+              }}
+            >
+              <h2 className="mail_heading" style={{ border: "1px solid" }}>
                 Check Your Email
               </h2>
               <p
-                className={styles.heading}
-                style={{ display: "flex", justifyContent: "center" }}
+                className={styles.mailheading}
+                style={{ border: "1px solid black" }}
+                // style={{ display: "flex", justifyContent: "center" }}
               >
                 We have sent you a reset password link on <br></br>
                 ***given mail***
               </p>
             </div>
             <div className={styles.logo}>
-              <img src="./assets/letter box.png" alt="Tg-logo"></img>
+              <img src="./assets/letterBox.png" alt="Tg-logo"></img>
             </div>
 
             <div className={styles.button_wrapper}>
