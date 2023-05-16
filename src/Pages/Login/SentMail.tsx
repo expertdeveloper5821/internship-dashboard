73643ee2c700c132641ab5abb7fc74b449a98ff8
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-05-16 12:34:01 +0530

    some-ui-change

--- src/Pages/Login/SentMail.tsx
+++ src/Pages/Login/SentMail.tsx
@@ -1,7 +1,4 @@
-import girlImage from "../../Images/girlimg.png";
-import technologo from "../../Images/Technologo.png";
-import gradient from "../../Images/Ellipse 179.png";
-import styles from "./Login.module.scss";
+import styles from "./auth.module.scss";
 
 export interface IAppProps {}
 
@@ -12,29 +9,26 @@ export function SentMail() {
         <div className={styles.bgcontainer}>
           <div className={styles.container}>
             <div className={styles.logo}>
-              <img src={technologo} alt="Tg-logo"></img>
+              <img src="./assets/Tehnogeticlogo.png" alt="Tg-logo"></img>
             </div>
 
             {/* Para reset */}
             <div>
               <h2>Check Your Email</h2>
-              <p className={styles.parareset}>
+              <p className={styles.heading}>
                 We have sent you a reset password link on <br></br> ***given
                 mail***
               </p>
             </div>
 
-            <div className={styles.buttoncontainer}>
+            <div className={styles.button_wrapper}>
               <button type="submit" className={styles.forgetbutton}>
                 Go to email
               </button>
             </div>
           </div>
-          <div className={styles.girlSection}>
-            <img src={girlImage} alt="girl_image"></img>
-            <div className={styles.linearBottom}>
-              <img src={gradient} alt="gradient"></img>
-            </div>
+          <div className={styles.girlImg_wrapper}>
+            <img src="./assets/GirlImg.png" alt="girlImg"></img>
           </div>
         </div>
       </div>
